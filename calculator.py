6f82007cd6aabb07799ed63156a78487026e11b4
akashx0780 <akashx825@gmail.com>
2026-01-07 11:31:24 +0530

    version2

--- calculator.py
+++ calculator.py
@@ -3,17 +3,24 @@ class calculator:
            return a + b
       def sub(self,a,b):
            return a - b
+      def mul(self,a,b):
+           return a * b
+      def div(self,a,b):
+           return a % b
+           
 
 calc=calculator()
 
 while True:
     print("1.addition")
     print("2.subtraction")
-    print("3.exit")
+    print("3.multiply")
+    print("4.division")
+    print("5.exit")
     
     choice=input("enter choice:")
     
-    if choice == "3":
+    if choice == "5":
       print("Existing calculator. Thank you")
       break
       
@@ -23,7 +30,11 @@ while True:
     if choice == "1":
       print("Result:",calc.add(a,b))
     elif choice == "2":
-      print("Result:",calc.sub(a,b)) 
+      print("Result:",calc.sub(a,b))
+    elif choice == "3":
+      print("Result:",calc.mul(a,b))
+    elif choice == "4":
+      print("Result:",calc.div(a,b)) 
       
     else:
         print("error")
